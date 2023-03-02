e9b469ecd779a063ffa86901f5a118eee0b91051
sezer <sezer@MAHIR>
2023-03-03 00:42:26 +0300

    Add project files.

---
+++ Deneme/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ödev
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+          
+           
+
+
+
+
+           
+        }
+
+    
+    }
+    
+}
